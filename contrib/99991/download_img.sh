8c21a215408778c43ab97fa9f5ca6421cb5c2e1f
HexagonPrime <43519981+HexagonPrime@users.noreply.github.com>
2021-05-25 10:18:43 +0100

    Update download_img.sh

--- contrib/99991/download_img.sh
+++ contrib/99991/download_img.sh
@@ -6,5 +6,6 @@ source /scratch_net/biwidl306/shecai/conda/etc/profile.d/conda.sh
 conda activate pi-gan
 # python3 ./download.py -d /scratch_net/biwidl306_second/shecai/hypersim --contains .color.hdf5 --silent
 # python3 ./download.py -d /scratch_net/biwidl306_second/shecai/hypersim --contains _detail --silent
-python3 ./download.py -d /scratch_net/biwidl306_second/shecai/hypersim --contains .diffuse_reflectance.hdf5 --silent
+# python3 ./download.py -d /scratch_net/biwidl306_second/shecai/hypersim --contains .diffuse_reflectance.hdf5 --silent
 # python3 ./download.py -d /scratch_net/biwidl306_second/shecai/hypersim --contains .position.hdf5 --silent
+python3 ./download.py -d /scratch_net/biwidl306_second/shecai/hypersim --contains .jpg --silent
