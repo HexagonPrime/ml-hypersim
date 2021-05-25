a13c05219a4b8d0506c12fffb821cc9c9f877575
HexagonPrime <43519981+HexagonPrime@users.noreply.github.com>
2021-05-25 17:51:00 +0100

    Update download_img.sh

--- contrib/99991/download_img.sh
+++ contrib/99991/download_img.sh
@@ -5,8 +5,8 @@
 source /scratch_net/biwidl306/shecai/conda/etc/profile.d/conda.sh
 conda activate pi-gan
 # python3 ./download.py -d /scratch_net/biwidl306_second/shecai/hypersim --contains .color.hdf5 --silent
-python3 ./download.py -d /scratch_net/biwidl306_second/shecai/hypersim --contains _detail --silent
+python3 ./download.py -d /scratch_net/biwidl306_second/shecai/hypersim_0 --contains _detail --silent
 # python3 ./download.py -d /scratch_net/biwidl306_second/shecai/hypersim --contains .diffuse_reflectance.hdf5 --silent
-python3 ./download.py -d /scratch_net/biwidl306_second/shecai/hypersim --contains .position.hdf5 --silent
-python3 ./download.py -d /scratch_net/biwidl306_second/shecai/hypersim --contains color.jpg --silent
-python3 ./download.py -d /scratch_net/biwidl306_second/shecai/hypersim --contains diffuse_reflectance.jpg --silent
+python3 ./download.py -d /scratch_net/biwidl306_second/shecai/hypersim_0 --contains .position.hdf5 --silent
+python3 ./download.py -d /scratch_net/biwidl306_second/shecai/hypersim_0 --contains color.jpg --silent
+python3 ./download.py -d /scratch_net/biwidl306_second/shecai/hypersim_0 --contains diffuse_reflectance.jpg --silent
