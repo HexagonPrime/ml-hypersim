f1b16647ed2e012b6a873d15cd10a5d14cd6dc6f
HexagonPrime <43519981+HexagonPrime@users.noreply.github.com>
2021-05-28 16:59:13 +0100

    Update remove.sh

--- contrib/99991/remove.sh
+++ contrib/99991/remove.sh
@@ -1,1 +1,7 @@
+#!/bin/bash -l
+#SBATCH --output=b.out
+#SBATCH --mem=40G
 
+source /scratch_net/biwidl306/shecai/conda/etc/profile.d/conda.sh
+conda activate pi-gan
+rm -rf /scratch_net/biwidl306_second/shecai/hypersim_0
