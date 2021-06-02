8ceb0d09810c442511306d916b50cfb77633338f
HexagonPrime <43519981+HexagonPrime@users.noreply.github.com>
2021-06-02 04:38:04 +0100

    Update download_single.sh

--- download_single.sh
+++ download_single.sh
@@ -4,9 +4,4 @@
 
 source /scratch_net/biwidl306/shecai/conda/etc/profile.d/conda.sh
 conda activate pi-gan
-# python3 ./download.py -d /scratch_net/biwidl306_second/shecai/hypersim --contains .color.hdf5 --silent
-# python3 ./download.py -d /scratch_net/biwidl306_second/shecai/hypersim --contains .diffuse_reflectance.hdf5 --silent
-# python3 ./download.py -d /scratch_net/biwidl306_second/shecai/hypersim --contains _detail --silent
-# python3 ./download.py -d /scratch_net/biwidl306_second/shecai/hypersim --contains .position.hdf5 --silent
-python3 ./download.py -d /scratch_net/biwidl306_second/shecai/hypersim --contains ai_027_008/images/scene_cam_01_final_preview/frame.0096.color.jpg --silent
-# python3 ./download.py -d /scratch_net/biwidl306_second/shecai/hypersim --contains diffuse_reflectance.jpg --silent
+python3 ./download.py -d /scratch_net/biwidl306_second/shecai/hypersim --contains ai_027_008/images/scene_cam_01_final_preview/frame.0096.color.jpg --silent --overwrite
